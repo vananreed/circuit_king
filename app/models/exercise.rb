f062f5b666cd8253a48dd6531a51e37e528c46ea
Reed Van Anrooy <reed.vananrooy@gmail.com>
2018-12-07 19:00:30 -0700

    validate presence of a name for cicruits

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,5 +1,5 @@
 class Exercise < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   has_many :circuit_exercises
   has_many :circuits, through: :circuit_exercises
 end
