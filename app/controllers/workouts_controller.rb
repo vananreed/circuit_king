4915104b456a010f09deaf66fa34c6ed0e2a3d3d
Reed Van Anrooy <reed.vananrooy@gmail.com>
2018-12-14 16:08:47 -0700

    controller and routes generated for workouts

---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,19 @@
+class WorkoutsController < ApplicationController
+
+  def create
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  private
+
+  def set_workout
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   resources :circuits do
     get 'workout', to: 'circuits#workout'
   end
+  resources :workouts,  only: [:create, :show, :index]
 end
