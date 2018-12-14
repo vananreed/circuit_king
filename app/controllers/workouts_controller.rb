e909b4f96b5cab3bbd6708dcde3db860cf422546
Reed Van Anrooy <reed.vananrooy@gmail.com>
2018-12-14 16:39:23 -0700

    workout views and seeds

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,19 +1,19 @@
 class WorkoutsController < ApplicationController
+  before_action :set_workout, only: [:show]
 
   def create
   end
 
   def show
-
   end
 
-  def create
-
+  def index
+    @workouts = Workout.where(user_id: current_user.id)
   end
 
   private
 
   def set_workout
-
+    @workout = Workout.find(params[:id])
   end
 end
--- app/models/circuit.rb
+++ app/models/circuit.rb
@@ -1,5 +1,5 @@
 class Circuit < ApplicationRecord
-  has_many :circuit_exercises
+  has_many :circuit_exercises, dependent: :destroy
   has_many :exercises, through: :circuit_exercises
   validates :name, presence: true
   has_many :workouts
